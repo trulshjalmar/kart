f1d6254b6a1548c40f78e4d643b1a915c315e4ca
trulshjalmar <86436012+trulshjalmar@users.noreply.github.com>
2023-12-17 03:15:48 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,7 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+author 'Julls Hjalmar <truls.hjalmar@hotmail.com>'
+description 'Norsk kart Fivem'
+version '1.6.9'
+
+client_script "client.lua"
